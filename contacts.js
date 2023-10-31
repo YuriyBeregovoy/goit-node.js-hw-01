03dc3e4927a85584d68b9bfae72f4f1a40152211
YuriyBeregovoy <rik0681@gmail.com>
2023-10-31 12:52:00 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -2,3 +2,12 @@ const fs = require('fs/promises');
 const path = require('path');
 
 const contactsPath = path.join(__dirname, 'contacts.json');
+
+function listContacts() {
+  try {
+    const data = fs.readFileSync(contactsPath, 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+}
