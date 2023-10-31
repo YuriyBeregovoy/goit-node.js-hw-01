20acce31bb0bef2d21f8f6edd28c7b2cf7886bbf
YuriyBeregovoy <rik0681@gmail.com>
2023-10-31 21:12:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import {
   addContact,
 } from "./contacts.js";
 
+const { Command } = require('commander');
+
 async function main() {
   // Вивести список контактів
   const allContacts = await listContacts();
@@ -23,3 +25,65 @@ async function main() {
 }
 
 main();
+
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      const allContacts = await listContacts();
+      console.log('Усі контакти:', allContacts);
+      break;
+
+    case 'get':
+      if (id) {
+        // Обробка дії "get" з id
+        const contactById = await getContactById(id);
+        if (contactById) {
+          console.log('Контакт за ID:', contactById);
+        } else {
+          console.log('Контакт не знайдений за вказаним ID.');
+        }
+      } else {
+        console.warn('ID is required for "get" action.');
+      }
+      break;
+
+    case 'add':
+      if (name && email && phone) {
+        const newContact = await addContact(name, email, phone);
+        console.log('Доданий контакт:', newContact);
+      } else {
+        console.warn('Name, email, and phone are required for "add" action.');
+      }
+      break;
+
+    case 'remove':
+      if (id) {
+        const removedContact = await removeContact(id);
+        if (removedContact) {
+          console.log('Видалений контакт:', removedContact);
+        } else {
+          console.log('Контакт не знайдений за вказаним ID.');
+        }
+      } else {
+        console.warn('ID is required for "remove" action.');
+      }
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
