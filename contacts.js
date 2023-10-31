0e8be028cca1f13ae96d05ca5e2baf1f42811df5
YuriyBeregovoy <rik0681@gmail.com>
2023-10-31 19:05:02 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,6 +1,6 @@
 
 
-import { readFileSync, writeFileSync } from 'fs/promises';
+import { promises  } from 'fs';
 import path from "path";
 import { v4 as uuidv4 } from 'uuid';
 const newId = uuidv4();
@@ -8,38 +8,30 @@ const newId = uuidv4();
 
 const contactsPath = path.resolve("./db/contacts.json");
 
-function listContacts() {
+export async function listContacts() {
   try {
-    const data = readFileSync(contactsPath, 'utf-8');
+    const data = await promises.readFile(contactsPath, 'utf-8');
     return JSON.parse(data);
   } catch (error) {
     return [];
   }
 }
 
-function getContactById(contactId) {
-  const contacts = listContacts();
-  const foundContact = contacts.find(contact => contact.id === contactId);
-  return foundContact || null;
-}
-
-function removeContact(contactId) {
-  const contacts = listContacts();
+async function removeContact(contactId) {
+  const contacts = await listContacts();
   const index = contacts.findIndex(contact => contact.id === contactId);
   if (index === -1) {
     return null;
   }
   const removedContact = contacts.splice(index, 1)[0];
-  writeFileSync(contactsPath, JSON.stringify(contacts, null, 2));
+  await promises.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return removedContact;
 }
 
-function addContact(name, email, phone) {
-  const contacts = listContacts();
+export async function addContact(name, email, phone) {
+  const contacts = await listContacts();
   const newContact = { id: newId, name, email, phone };
   contacts.push(newContact);
-  writeFileSync(contactsPath, JSON.stringify(contacts, null, 2));
+  await promises.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return newContact;
 }
-
-export default { listContacts, getContactById, removeContact, addContact };
